bbee8a043fc7ecda269a5a60e58e247c8777d4a8
GoByeonghu <byeonghugo@gmail.com>
2024-06-27 16:33:54 +0900

    Initial commit: Set up basic Spring project structure

---
+++ Decide4Me/src/main/java/com/sfz/rest_api/RestAPIApplication.java
@@ -0,0 +1,7 @@
+package com.sfz.rest_api;
+
+public class RestAPIApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ Decide4Me/src/main/java/com/sfz/rest_api/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.sfz.rest_api.controller;
+
+public class UserController {
+}
